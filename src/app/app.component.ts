a89e6a94c960938d978cf00305724d5afff44bc6
fernandobalbendea <fernando.b.albendea@accenture.com>
2020-04-13 13:01:55 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component,  OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+declare var jQuery: any;
+
 
 @Component({
   selector: 'my-app',
@@ -9,6 +12,25 @@ import { Component,  OnInit } from '@angular/core';
 
 
 export class AppComponent  {
+
+parse(text){
+     var json = JSON.parse(text)
+
+
+
+    document.getElementById("alert_total_count_cage").innerHTML = json[0].mechanical_cases.number + json[0].electrical_cases.number + json[0].structural_cases.number
+
+    document.getElementById("alert_total_count_st40").innerHTML = json[1].mechanical_cases.number + json[1].electrical_cases.number + json[1].structural_cases.number
+
+    document.getElementById("st40_struct").innerHTML = json[1].structural_cases.number
+    document.getElementById("st40_mec").innerHTML = json[1].mechanical_cases.number
+    document.getElementById("st40_elec").innerHTML = json[1].electrical_cases.number
+
+    document.getElementById("cage_struct").innerHTML = json[0].structural_cases.number
+    document.getElementById("cage_mec").innerHTML = json[0].mechanical_cases.number
+    document.getElementById("cage_elec").innerHTML = json[0].electrical_cases.number
+
+}
 
   ngOnInit(){
     var text = '['+
@@ -43,25 +65,65 @@ export class AppComponent  {
 '}'+
 '}'+
 ']'
-var json = JSON.parse(text)
 
-document.getElementById("alert_total_count_cage").innerHTML += json[0].mechanical_cases.number + json[0].electrical_cases.number + json[0].structural_cases.number
+this.parse(text)
+  }
+/*
+var Url = "http://192.168.221.130:8081/get_dizmo_data?location=Operating&type=ME";
+      
+      (function ($) {
+      $.ajax({
+            url: Url,
+            type:"GET",
+            success: function(result){
+                this.parse(result)
+            },
+            error: function(error){
+                document.getElementById('ME').innerHTML = error;
+            }
+        })
+    })(jQuery);
 
-document.getElementById("alert_total_count_st40").innerHTML += json[1].mechanical_cases.number + json[1].electrical_cases.number + json[1].structural_cases.number
 
-document.getElementById("st40_struct").innerHTML += json[1].structural_cases.number
-document.getElementById("st40_mec").innerHTML += json[1].mechanical_cases.number
-document.getElementById("st40_elec").innerHTML += json[1].electrical_cases.number
+var socket = new WebSocket("ws://192.168.221.130:8080/jsr356toUpper");
+        
+socket.onopen = function(e) {
+  socket.send("Dizmo");
+ };
+        
+socket.onmessage = function(e){
+  var json = JSON.parse(e.data)
 
-document.getElementById("cage_struct").innerHTML += json[0].structural_cases.number
-document.getElementById("cage_mec").innerHTML += json[0].mechanical_cases.number
-document.getElementById("cage_elec").innerHTML += json[0].electrical_cases.number
 
 
+  document.getElementById("alert_total_count_cage").innerHTML = json[0].mechanical_cases.number + json[0].electrical_cases.number + json[0].structural_cases.number
+
+  document.getElementById("alert_total_count_st40").innerHTML = json[1].mechanical_cases.number + json[1].electrical_cases.number + json[1].structural_cases.number
+
+  document.getElementById("st40_struct").innerHTML = json[1].structural_cases.number
+  document.getElementById("st40_mec").innerHTML = json[1].mechanical_cases.number
+  document.getElementById("st40_elec").innerHTML = json[1].electrical_cases.number
+
+  document.getElementById("cage_struct").innerHTML = json[0].structural_cases.number
+  document.getElementById("cage_mec").innerHTML = json[0].mechanical_cases.number
+  document.getElementById("cage_elec").innerHTML = json[0].electrical_cases.number
+
+ 
+}
+
+ 
 
   }
+
+
+
+
+  
+
+
 
   display(){
-    document.getElementById("Tabla").innerHTML += "Hola"
-  }
+    
+}
+*/
 }
