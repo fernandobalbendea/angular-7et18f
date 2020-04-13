92377265d1ad26edf2a8d3ddbd3316172319a531
fernandobalbendea <fernando.b.albendea@accenture.com>
2020-04-13 11:00:05 +0200

    Cambios lunes 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './Style/app.component.css' ]
 })
+
+
+
 export class AppComponent  {
-  name = 'Angular';
+
+  ngOnInit(){
+    var text = '['+
+'{'+
+'"location":"cage01",'+
+'"mechanical_cases":{'+
+'"number":10,'+
+'"alerted":"yes"'+
+'},'+
+'"electrical_cases":{'+
+'"number":10,'+
+'"alerted":"yes"'+
+'},'+
+'"structural_cases":{'+
+'"number":10,'+
+'"alerted":"yes"'+
+'}'+
+'},'+
+'{'+
+'"location":"st40",'+
+'"mechanical_cases":{'+
+'"number":2,'+
+'"alerted":"yes"'+
+'},'+
+'"electrical_cases":{'+
+'"number":3,'+
+'"alerted":"yes"'+
+'},'+
+'"structural_cases":{'+
+'"number":4,'+
+'"alerted":"no"'+
+'}'+
+'}'+
+']"'
+var json = JSON.parse(text)
+
+
+  }
 
   display(){
     document.getElementById("Tabla").innerHTML += "Hola"
